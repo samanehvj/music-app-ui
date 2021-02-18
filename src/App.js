36fc1b61d44946c7e3dd6de6678ecc2b9c5e9733
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-02-17 17:31:02 -0800

    Fix audio changer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
 import React, {useState, useEffect} from "react";
-import styles from "./App.css";
+import "./App.css";
 
 import {Card, Image, ListGroup} from "react-bootstrap";
 
 const App = () => {
   const [songs, setSongs] = useState([]);
-  const [playing, setPlaying] = useState({});
+  const [playing, setPlaying] = useState({id:1, name:"Hello", singer:"Adele", img:"adele.png", type:"Pop", mp3:"Adele.mp3"});
   const server_url = `http://localhost:8765`;
+
+  const [updatePlayer, setUpdatePlayer] = useState(Math.random);
 
   useEffect(() => {
     fetch(server_url)
     .then(res => res.json())
     .then(jsonRes => {
-      
-      setPlaying(jsonRes.songs[0]);
-      setSongs(jsonRes.songs);
-      // jsonRes.songs.map(song => setSongs([...song]))
-      // console.log('Songs are: ', jsonRes.songs);
+        setPlaying(jsonRes.songs[0]);
+        setSongs(jsonRes.songs);
     });
-    
+    return;
   }, []);
-  // console.log('songs:', songs)
-  // console.log('playing:', playing)
+
+  const changeSong = (song) => {
+    console.log("LOL",song)
+    setPlaying(song)
+  }
+
+
+  useEffect(() => {
+   if(playing){
+     setUpdatePlayer(Math.random);
+   }
+  }, [playing]);
+  
   return (
     <div className="container">
       <div className="logo centercontent" >
       <Image src="./logo.svg" width="300" />
       </div>
       <div className="centercontent" >
-        <Card style={{ width: '20rem', }}>
+        <Card style={{ width: '20rem', }} key={updatePlayer}>
           <Image className="img" width="300" height="300" src={`${server_url}/music/imgs/${playing.img}`} roundedCircle />
           <Card.Body>
             <Card.Title className="title">{playing.name}</Card.Title>
@@ -38,17 +48,12 @@ const App = () => {
             <Card.Text className="type">
               Type: {playing.type}
             </Card.Text>
-            <audio controls autoPlay className="">
+            <audio controls autoPlay>
               <source src={`${server_url}/music/mp3/${playing.mp3}`} type="audio/mpeg" />
               Your browser does not support the audio element.
             </audio>
           </Card.Body>
         </Card>
-        {/* <img src={`${server_url}/music/imgs/${playing.img}`} width="200" height="200" />
-        <audio controls>
-          <source src={`${server_url}/music/mp3/${playing.mp3}`} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio> */}
       </div>
       <div className="centercontent" >
         <ListGroup className="list">
@@ -56,7 +61,7 @@ const App = () => {
         songs.map(song => {
           let clsName = (song.id == playing.id) ? 'active' : ''; 
           return (
-            <ListGroup.Item className={clsName} key={song.id} onClick={async () => await setPlaying(song)}>{song.name}</ListGroup.Item>
+            <ListGroup.Item className={clsName} key={song.id} onClick={ () =>  changeSong(song)}>{song.name}</ListGroup.Item>
           )
         })
         }

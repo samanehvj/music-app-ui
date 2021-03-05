a5fee5acd3b88a27999ff600668c8ce2b6b63fd6
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-03-04 23:08:32 -0800

    Set Endpoint and server url

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import "./styles/logo.css";
 
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://localhost:3000";
-const server_url = "http://localhost:3000";
-// const ENDPOINT = "https://musicapp-nodejs-mysql.herokuapp.com/";
-// const server_url = `https://musicapp-nodejs-mysql.herokuapp.com/`;
+// const ENDPOINT = "http://localhost:3000";
+// const server_url = "http://localhost:3000";
+const ENDPOINT = "https://musicapp-nodejs-mysql.herokuapp.com/";
+const server_url = `https://musicapp-nodejs-mysql.herokuapp.com/`;
 
 const App = () => {
   const socket = socketIOClient(ENDPOINT);
